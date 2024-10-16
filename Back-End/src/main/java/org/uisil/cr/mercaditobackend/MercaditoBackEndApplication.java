67b99dcd0e19a218ae23180814b23eb68c43b4ab
Fauricio salazar vega <fausalazar10@gmail.com>
2024-10-15 18:22:14 -0600

    Inicio Back-end

---
+++ Back-End/src/main/java/org/uisil/cr/mercaditobackend/MercaditoBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.uisil.cr.mercaditobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaditoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaditoBackEndApplication.class, args);
+	}
+
+}
